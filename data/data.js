2a018caa3d1ec615ff13307d07a50b80d7bdeb70
Barjua <47339114+Barjua@users.noreply.github.com>
2019-02-28 21:45:11 -0500

    Update data.js

--- data/data.js
+++ data/data.js
@@ -2,7 +2,7 @@ var MFCOMPANYID = 'C6d6401f91cf82e30908681a4abbc726f';
 
 var MFPROJECTID = 'Mc91fcb556daf217407ee4d0d01ea478e1536629355079';
 
-
+var mfprojectdata = '<wireframe><gridSettings><pageCommon  snapToGrid=\'false\'  snapPos=\'5\'  backgroundColor=\'#ffffff\'  ></pageCommon></gridSettings><mffontcollection></mffontcollection><favourite-colors></favourite-colors> <component type="Page"  eid="aba8760528a74d71af27db2aa6c4ff05"  pageName="Page 1"  parentPage=""  masterPage=""  folder="false"  navHide="false"  ></component></wireframe>';
 
 var MFPAGEOBJARR = new Object();
 
